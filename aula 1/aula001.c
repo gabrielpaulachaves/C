67f73dda0bf6f825ea3118f9bed10b9d7d2643fb
Gabriel <bielpchaves@gmail.com>
2025-09-26 22:47:31 -0300

    c

--- aula 1/aula001.c
+++ aula 1/aula001.c
@@ -3,14 +3,13 @@
 
 
 int main(){
-    int inteiro;
-float decimal;
-char caracter;
+     int inteiro;
+    float decimal;
+    char caracter;
 
 printf("Informe um numero:");
 scanf("%d", &inteiro);
 printf("voce escolheu: %d" , inteiro);
 
-system('Pause');
 }
 
