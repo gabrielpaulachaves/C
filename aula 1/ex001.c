a5f9d6641f0f35c665b7349c2f817d9f9107e1cf
Gabriel <bielpchaves@gmail.com>
2025-09-26 23:15:01 -0300

    c

---
+++ aula 1/ex001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Usando a ESC no printf */
+int main(){
+printf("Valor inteiro: %d\n", 10);
+printf("valor decimal: %f\n", 3.1419847);
+printf("Valor decimal só com duas casas: %.2f\n", 3.1419847);
+printf("Texto: %s", "Teste");
+
+}
+
+/*os aspas simples '' são para caractere, e os aspas duplas "" sao para caracteres*/
