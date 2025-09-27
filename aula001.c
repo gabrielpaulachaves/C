262e35ebe16fea0241375634e77647b39baa2aa2
Gabriel <bielpchaves@gmail.com>
2025-09-26 22:21:31 -0300

    c

---
+++ aula001.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int inteiro;
+float decimal;
+char caracter;
+
+printf("Informe um numero:");
+scanf("%d", &inteiro);
+printf("voce escolheu: %d" , inteiro);
+
+system('Pause');
+}
+
