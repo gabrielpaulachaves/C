2abd7ec42d90d1fa476b281396e84e2d5e9b0f2f
Gabriel <bielpchaves@gmail.com>
2025-10-10 10:45:36 -0300

    c

--- aula 3/ex001.c
+++ aula 3/ex001.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-
+#define texto "Entrada e saida de dado"
 /*Estrutura de decisão*/
 int main(void){
 
 
 float nota;
+printf("%s\n", texto);
 printf("Digite sua nota: ");
 scanf("%f", &nota);
 if(nota >= 5){
