77a577b48fddaf183e9b7abdbc090a306e2fead3
Gabriel <bielpchaves@gmail.com>
2025-10-17 11:42:21 -0300

    c

---
+++ Conteúdo/aula 4/ex001.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/*Aprendendo a usar WHILE */
+int main (void){
+    
+    int i = 0;
+    
+    while(i <= 10){
+        printf("%d ", i++);
+        i++;
+    }
+
+
+
+    return 0;
+}
---
+++ Conteúdo/aula 4/ex002.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*usando do-while*/
+int main(void){
+
+    int valor = 1;
+
+    do{
+        printf("%d ", valor);
+        valor++;
+    }while(valor <= 20);
+    
+
+
+    return 0;
+}
